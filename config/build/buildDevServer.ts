9c19465c0060c04a2de2f1c278c09fd4be96ba7d
ASVVlasov <asvvlasov@gmail.com>
2023-02-19 01:26:17 +0300

    test lint

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -2,6 +2,7 @@ import type { Configuration as DevServerConfiguration } from 'webpack-dev-server
 import { type IBuildOptions } from './types/config';
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
+    const a = 5;
     return {
         port: options.port,
         open: true,
