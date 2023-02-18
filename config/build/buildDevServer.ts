80f40789427fefafcb1c302d917130cce6ce4a31
ASVVlasov <asvvlasov@gmail.com>
2023-02-19 01:46:16 +0300

    cache

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -2,7 +2,6 @@ import type { Configuration as DevServerConfiguration } from 'webpack-dev-server
 import { type IBuildOptions } from './types/config';
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
-    const a = 5;
     return {
         port: options.port,
         open: true,
