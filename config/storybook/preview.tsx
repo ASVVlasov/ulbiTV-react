23c732fa17eb34417ba94ca254992fd589757473
ASVVlasov <asvvlasov@gmail.com>
2023-02-19 20:44:06 +0300

    loki

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,8 +1,6 @@
 import { StyleDecorator } from 'shared/lib/storybook/StyleDecorator';
 import { RouterDecorator } from 'shared/lib/storybook/RouterDecorator';
 
-import { type DecoratorFn } from '@storybook/react';
-
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     layout: 'centered',
@@ -14,27 +12,4 @@ export const parameters = {
     },
 };
 
-export const globalTypes = {
-    theme: {
-        name: 'Theme',
-        description: 'Global theme for components',
-        defaultValue: 'light',
-        toolbar: {
-            icon: 'circlehollow',
-            // Array of plain string values or MenuItem shape (see below)
-            items: ['light', 'dark', 'blue'],
-            // Property that specifies if the name of the item will be displayed
-            title: 'Theme',
-            // Change title based on selected value
-            dynamicTitle: false,
-        },
-    },
-};
-
-const withTheme: DecoratorFn = (StoryFn, context) => (
-    <div className={`app ${context.globals.theme as string}`}>
-        <StoryFn />
-    </div>
-);
-
-export const decorators = [withTheme, StyleDecorator, RouterDecorator];
+export const decorators = [StyleDecorator, RouterDecorator];
