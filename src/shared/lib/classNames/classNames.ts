6a34bc4892a85c8606131f94e567110223981c08
Aleksandr <asvvlasov@gmail.com>
2023-03-13 21:56:13 +0300

    edit profile, avatar

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,15 +1,13 @@
 type TMods = Record<string, boolean | string>;
 
-export function classNames(
-    cls: string,
-    mods: TMods = {},
-    additional: Array<string | undefined> = []
-): string {
-    return [
-        cls,
-        ...additional,
-        ...Object.entries(mods)
-            .filter(([, value]) => Boolean(value))
-            .map(([className]) => className),
-    ].filter(Boolean).join(' ');
+export function classNames(cls: string, mods: TMods = {}, additional: Array<string | undefined> = []): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([, value]) => Boolean(value))
+      .map(([className]) => className),
+  ]
+    .filter(Boolean)
+    .join(' ');
 }
